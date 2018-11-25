a075b7c43af67eae2165d49cdc48a65173a81f2e
Mark Markov <digitallymarked@gmail.com>
2018-11-25 20:57:27 +0200

    Updated omnibar to new layout

--- src/graphics/omnibar/omnibar.js
+++ src/graphics/omnibar/omnibar.js
@@ -4,14 +4,10 @@ import { TimelineLite, Power2 } from 'gsap/all'
 import 'normalize.css'
 import './omnibar.scss'
 
-const streamTopic = nodecg.Replicant('streamTopic')
-
 class Omnibar extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      topic: 'Just starting!',
-    }
+    this.state = {}
     // reference to the DOM node
     this.myElement = null
     // reference to the animation
@@ -19,28 +15,22 @@ class Omnibar extends Component {
   }
 
   componentDidMount() {
-    // Listen for changes and update when the Replicant value changes
-    streamTopic.on('change', newValue => {
-      // use the node ref to create the animation
-      const changeState = () => {
-        this.setState({ topic: newValue })
-      }
-      this.myTween
-        .to(this.myElement, 0.3, { y: -15, opacity: 0, ease: Power2.easeIn })
-        .call(changeState)
-        .to(this.myElement, 0.3, { y: 0, opacity: 1, ease: Power2.easeOut })
-    })
-  }
-
-  componentWillUnmount() {
-    // ? Unlisten so that updates don't trigger alerts
-    nodecg.unlisten('streamTopic')
+    // // Listen for changes and update when the Replicant value changes
+    // streamTopic.on('change', newValue => {
+    //   // use the node ref to create the animation
+    //   const changeState = () => {
+    //     this.setState({ topic: newValue })
+    //   }
+    //   this.myTween
+    //     .to(this.myElement, 0.3, { y: -15, opacity: 0, ease: Power2.easeIn })
+    //     .call(changeState)
+    //     .to(this.myElement, 0.3, { y: 0, opacity: 1, ease: Power2.easeOut })
+    // })
   }
 
   render() {
     return (
       <div id="omnibar">
-        <p ref={p => (this.myElement = p)}>{this.state.topic}</p>
         <div className="background" />
       </div>
     )
