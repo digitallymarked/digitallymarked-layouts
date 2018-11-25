92ecb469a9872e96c50fccd23b7794c8d3534487
Mark Markov <digitallymarked@gmail.com>
2018-11-25 13:55:57 +0200

    Added editor configuration, linting and prettier and refactored accordingly

--- src/dashboard/components/Topic.js
+++ src/dashboard/components/Topic.js
@@ -1,55 +1,52 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
-const streamTopic = nodecg.Replicant("streamTopic");
+import './Topic.scss'
 
-import "./Topic.scss";
+const streamTopic = nodecg.Replicant('streamTopic')
 
 export default class Topic extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      currentTopic: null
-    };
+      currentTopic: null,
+    }
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  //Getting the value of the input field
-  handleChange(event) {
-    this.setState({ currentTopic: event.target.value });
-  }
-
-  //Sending the value to the replicant
-  handleSubmit(event) {
-    event.preventDefault();
-    streamTopic.value = this.state.currentTopic;
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   componentDidMount() {
-    //Load initial value and set it's propper state
-    nodecg.readReplicant("streamTopic", value => {
-      this.setState({ currentTopic: value });
-    });
+    // Load initial value and set it's propper state
+    nodecg.readReplicant('streamTopic', value => {
+      this.setState({ currentTopic: value })
+    })
 
-    //Listen for changes and update when the Replicant value changes
-    streamTopic.on("change", newValue => {
-      this.setState({ currentTopic: newValue });
-    });
+    // Listen for changes and update when the Replicant value changes
+    streamTopic.on('change', newValue => {
+      this.setState({ currentTopic: newValue })
+    })
+  }
+
+  // Getting the value of the input field
+  handleChange(event) {
+    this.setState({ currentTopic: event.target.value })
+  }
+
+  // Sending the value to the replicant
+  handleSubmit(event) {
+    event.preventDefault()
+    streamTopic.value = this.state.currentTopic
   }
 
   render() {
     return (
       <div className="topic">
+        <h1>Stream Topic</h1>
         <form onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            value={this.state.currentTopic}
-            onChange={this.handleChange}
-          />
+          <input type="text" value={this.state.currentTopic} onChange={this.handleChange} />
           <input type="submit" value="Change Topic" />
         </form>
       </div>
-    );
+    )
   }
 }
---
+++ src/dashboard/dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Topic from './components/Topic'
+
+ReactDOM.render(
+  <div>
+    <Topic />
+  </div>,
+  document.getElementById('root')
+)
