365983528f9e13ee0259b9fef24944481f684852
Mark Markov <digitallymarked@gmail.com>
2018-11-25 22:19:13 +0200

    Made omnibar a stateless component only to render

--- src/graphics/omnibar/omnibar.js
+++ src/graphics/omnibar/omnibar.js
@@ -1,40 +1,45 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
-import { TimelineLite, Power2 } from 'gsap/all'
 import 'normalize.css'
+import '../_common.scss'
 import './omnibar.scss'
 
-class Omnibar extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
-    // reference to the DOM node
-    this.myElement = null
-    // reference to the animation
-    this.myTween = new TimelineLite()
-  }
+// class Omnibar extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {}
+//     // reference to the DOM node
+//     this.myElement = null
+//     // reference to the animation
+//     this.myTween = new TimelineLite()
+//   }
 
-  componentDidMount() {
-    // // Listen for changes and update when the Replicant value changes
-    // streamTopic.on('change', newValue => {
-    //   // use the node ref to create the animation
-    //   const changeState = () => {
-    //     this.setState({ topic: newValue })
-    //   }
-    //   this.myTween
-    //     .to(this.myElement, 0.3, { y: -15, opacity: 0, ease: Power2.easeIn })
-    //     .call(changeState)
-    //     .to(this.myElement, 0.3, { y: 0, opacity: 1, ease: Power2.easeOut })
-    // })
-  }
+//   componentDidMount() {
+//     // Listen for changes and update when the Replicant value changes
+//     streamTopic.on('change', newValue => {
+//       // use the node ref to create the animation
+//       const changeState = () => {
+//         this.setState({ topic: newValue })
+//       }
+//       this.myTween
+//         .to(this.myElement, 0.3, { y: -15, opacity: 0, ease: Power2.easeIn })
+//         .call(changeState)
+//         .to(this.myElement, 0.3, { y: 0, opacity: 1, ease: Power2.easeOut })
+//     })
+//   }
 
-  render() {
-    return (
-      <div id="omnibar">
-        <div className="background" />
-      </div>
-    )
-  }
-}
+//   render() {
+//     return (
+//       <div id="omnibar">
+//         <div className="background" />
+//       </div>
+//     )
+//   }
+// }
 
-ReactDOM.render(<Omnibar />, document.getElementById('root'))
+ReactDOM.render(
+  <div id="omnibar">
+    <div className="background" />
+  </div>,
+  document.getElementById('root')
+)
