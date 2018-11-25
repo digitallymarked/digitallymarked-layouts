f0a8f97f2862c0bb5501133f470ba92ba8a7a761
Mark Markov <digitallymarked@gmail.com>
2018-11-25 21:54:25 +0200

    FullCam layout added and refactored for correct output

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -1,8 +1,8 @@
-const {fork} = require('child_process');
+const { fork } = require('child_process')
 
-const isProduction = process.env.NODE_ENV === 'production';
-const command = isProduction ? 'build' : 'watch';
-const detailedReport = isProduction ? ['--detailed-report'] : [];
+const isProduction = process.env.NODE_ENV === 'production'
+const command = isProduction ? 'build' : 'watch'
+const detailedReport = isProduction ? ['--detailed-report'] : []
 
 // fork('./node_modules/.bin/parcel', [
 // 	command,
@@ -17,21 +17,21 @@ const detailedReport = isProduction ? ['--detailed-report'] : [];
 // ])
 
 fork('./node_modules/.bin/parcel', [
-	command,
-	'src/dashboard/*.html',
-	'--out-dir',
-	'dashboard',
-	'--public-url',
-	'./',
-	...detailedReport,
+  command,
+  'src/dashboard/*.html',
+  '--out-dir',
+  'dashboard',
+  '--public-url',
+  './',
+  ...detailedReport,
 ])
 
 fork('./node_modules/.bin/parcel', [
-	command,
-	'src/graphics/*/*.html',
-	'--out-dir',
-	'graphics',
-	'--public-url',
-	'./',
-	...detailedReport,
+  command,
+  'src/graphics/*.html',
+  '--out-dir',
+  'graphics',
+  '--public-url',
+  './',
+  ...detailedReport,
 ])
---
+++ src/graphics/fullCam/fullCam.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import 'normalize.css'
+import './fullCam.scss'
+
+ReactDOM.render(
+  <div id="fullCam">
+    <div id="background" />
+    <div id="components-wrapper" />
+  </div>,
+  document.getElementById('root')
+)
