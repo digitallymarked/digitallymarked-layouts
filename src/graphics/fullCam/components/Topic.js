1d2afecdfcabdde9157637c94c57f4fc14af801a
Mark Markov <digitallymarked@gmail.com>
2018-11-28 19:58:20 +0200

    Working topic with on/off toggle

--- src/graphics/fullCam/components/Topic.js
+++ src/graphics/fullCam/components/Topic.js
@@ -4,54 +4,56 @@ import { TimelineLite, Power2 } from "gsap/all";
 import "./Topic.module.scss";
 
 const streamTopic = nodecg.Replicant("streamTopic");
+const toggleCheck = nodecg.Replicant("toggleCheck");
 
 export default class Topic extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      topic: streamTopic.value
+      topic: ""
     };
+
     // reference to the DOM node
     this.myElement = null;
     this.myWrapper = null;
+
     // reference to the animation
-    this.myTween = new TimelineLite();
+    this.myTween = new TimelineMax();
   }
 
   componentDidMount() {
-    // Listen for changes and update when the Replicant value changes
-    streamTopic.on("change", (newValue, oldValue) => {
+    NodeCG.waitForReplicants(streamTopic, toggleCheck).then(() => {
+      this.setState({ topic: streamTopic.value });
+      // Listen for changes and update when the Replicant value changes
+      streamTopic.on("change", newValue => {
+        const changeState = () => {
+          this.setState({ topic: newValue });
+        };
+        this.myTween
+          .to(this.myElement, 0.3, {
+            y: -15,
+            opacity: 0,
+            ease: Power2.easeIn
+          })
+          .call(changeState)
+          .set(this.myElement, { y: 15 })
+          .to(this.myElement, 0.3, {
+            y: 0,
+            opacity: 1,
+            ease: Power2.easeOut
+          });
+      });
+    });
 
-      if (newValue == "" || newValue == undefined) {
-        //If input is empty, animate the component out
-        this.myTween.to(this.myWrapper, 0.3, {
-          opacity: 0,
-          y: 10,
-          ease: Power2.easeIn
-        });
-      } else if (oldValue == "" || oldValue == undefined) {
-        //If input WAS empty and there is a new topic, animate the component in
-        const changeState = () => {
-          this.setState({ topic: newValue });
-        };
+    // Listen if toggle is on or not
+    toggleCheck.on("change", newValue => {
+      this.setState({ topic: streamTopic.value });
+      if (newValue) {
         this.myTween
-          .call(changeState)
-          .fromTo(
-            this.myWrapper,
-            0.3,
-            { opacity: 0, y: 10 },
-            { opacity: 1, y: 0, ease: Power2.easeOut }
-          )
-          .to(this.myElement, 0.3, { y: 0, opacity: 1, ease: Power2.easeOut });
+          .set(this.myWrapper, { y: 15 })
+          .to(this.myWrapper, 0.3, { y: 0, opacity: 1 });
       } else {
-        //If the component is rendered and there is a change, play the normal animation
-        const changeState = () => {
-          this.setState({ topic: newValue });
-        };
-        this.myTween
-          .to(this.myElement, 0.3, { y: -15, opacity: 0, ease: Power2.easeIn })
-          .call(changeState)
-          .to(this.myElement, 0.3, { y: 0, opacity: 1, ease: Power2.easeOut });
+        this.myTween.to(this.myWrapper, 0.3, { y: 15, opacity: 0 });
       }
     });
   }
