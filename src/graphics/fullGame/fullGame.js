eaaac738933f511bbbf8c84dd658c7e6185ecb82
Mark Markov <digitallymarked@gmail.com>
2019-02-10 21:08:13 +0200

    Added Topic to Full Game screen as well

--- src/graphics/fullGame/fullGame.js
+++ src/graphics/fullGame/fullGame.js
@@ -5,11 +5,16 @@ import 'normalize.css'
 import './fullGame.scss'
 
 import background from './fullGame-background.png'
+import Topic from '../components/Topic'
 
 ReactDOM.render(
   <div id="fullGame">
     <div className="background" style={{ backgroundImage: `url(${background})` }} />
-    <div className="components-wrapper" />
+    <div className="components-wrapper">
+    </div>
+    <div className="gameWindow-wrapper">
+      <Topic />
+    </div>
   </div>,
   document.getElementById('root')
 )
