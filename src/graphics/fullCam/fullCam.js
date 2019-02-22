4a3c9ce92e92105cd3aa87087575e0d84ee492b7
Mark Markov <digitallymarked@gmail.com>
2019-02-22 10:44:43 +0200

    Added donation goal to Full Cam scene

--- src/graphics/fullCam/fullCam.js
+++ src/graphics/fullCam/fullCam.js
@@ -1,22 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import 'normalize.css'
-import './fullCam.scss'
+import 'normalize.css';
+import './fullCam.scss';
 
-import background from './fullCam-background.png'
-import Topic from '../components/Topic'
+import background from './fullCam-background.png';
+import Topic from '../components/Topic';
+
+import { url } from '../../../stream-assets/donationGoal';
 
 const FullCam = () => (
   <div id="fullCam">
-  <div className="background" style={{ backgroundImage: `url(${background})` }} />
-  <div className="components-wrapper">
-    <Topic/>
+    <div
+      className="background"
+      style={{ backgroundImage: `url(${background})` }}
+    />
+    <div className="components-wrapper">
+      <iframe
+        src={url}
+        referrerPolicy="no-referrer"
+        frameborder="0"
+        id="donationGoal"
+      />
+      <Topic />
+    </div>
   </div>
-</div>
-)
+);
 
-ReactDOM.render(
- <FullCam/>,
-  document.getElementById('root')
-)
+ReactDOM.render(<FullCam />, document.getElementById('root'));
