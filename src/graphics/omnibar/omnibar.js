e44095147a53c3fe47304b8e67ff95d8105fc497
Mark Markov <digitallymarked@gmail.com>
2019-02-28 17:58:34 +0200

    Added ncg-spotify bundle and reading

--- src/graphics/omnibar/omnibar.js
+++ src/graphics/omnibar/omnibar.js
@@ -1,10 +1,59 @@
-import React from 'react'
+import React, { Component } from "react";
+import { TimelineMax, Power2 } from "gsap/all";
 import ReactDOM from 'react-dom'
 
 import 'normalize.css'
 import '../_styles/_common.scss'
 
 import background from './omnibar-background.png'
+
+//NodeCG replicant for connecting to Spotify api - https://github.com/EwanLyon/ncg-spotify
+const songRep = nodecg.Replicant('currentSong', 'ncg-spotify');
+
+class CurrentSong extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      artist: '',
+      playing: false
+    }
+
+    // reference to the DOM node
+    this.myElement = null;
+    this.myWrapper = null;
+
+    // reference to the animation
+    this.myTween = new TimelineMax();
+  }
+
+  componentDidMount() {
+    songRep.on('change', newVal => {
+      this.setState({
+        name: newVal.name,
+        artist: newVal.artist,
+        playing: newVal.playing
+      })
+    });
+  }
+  render() {
+    return (
+      <div id="currentSong" ref={div => (this.myWrapper = div)}>
+        <div className="value" ref={div => (this.myElement = div)}>
+        {this.state.artist}: {this.state.name}
+        </div>
+      </div>
+    )
+  }
+}
+
+
+const Omnibar = () => (
+  <div id="omnibar">
+    <div className="background" style={{ backgroundImage: `url(${background})` }} />
+    <CurrentSong/>
+  </div>
+)
 // class Omnibar extends Component {
 //   constructor(props) {
 //     super(props)
@@ -39,8 +88,6 @@ import background from './omnibar-background.png'
 // }
 
 ReactDOM.render(
-  <div id="omnibar">
-    <div className="background" style={{ backgroundImage: `url(${background})` }} />
-  </div>,
+  <Omnibar/>,
   document.getElementById('root')
 )
