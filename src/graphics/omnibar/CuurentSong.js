13bbd5f81ff61579569148204ae99c9bb10e3c79
Mark Markov <digitallymarked@gmail.com>
2019-03-01 14:33:36 +0200

    Fixed issues with updates to ncg-spotify

--- src/graphics/omnibar/CuurentSong.js
+++ src/graphics/omnibar/CuurentSong.js
@@ -1,48 +1,74 @@
-import React, { Component } from 'react'
-import { TimelineMax, Power2 } from 'gsap/all'
-import OmniPill from '../components/OmniPill'
+import React, { Component } from 'react';
+import { TimelineMax, Power2 } from 'gsap/all';
+import OmniPill from '../components/OmniPill';
 
-import music from './icons/music.svg'
+import music from './icons/music.svg';
 
 // NodeCG replicant for connecting to Spotify api - https://github.com/EwanLyon/ncg-spotify
-const songRep = nodecg.Replicant('currentSong', 'ncg-spotify')
+const songRep = nodecg.Replicant('currentSong', 'ncg-spotify');
 
 class CurrentSong extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       name: '',
       artist: '',
       playing: false,
-    }
+    };
 
     // reference to the DOM node
-    this.myWrapper = null
+    this.myWrapper = null;
 
     // reference to the animation
-    this.myTween = new TimelineMax()
+    this.myTween = new TimelineMax();
   }
 
   componentDidMount() {
-    songRep.on('change', newVal => {
-      const { name, artist, playing } = newVal
+    //Wait for Replicant to be declared
+    NodeCG.waitForReplicants(songRep).then(() => {
+      //Load initial state of the items
+      nodecg.readReplicant('currentSong', 'ncg-spotify', value => {
+        this.setState({
+          name: value.name,
+          artist: value.artist,
+          playing: value.playing,
+        });
+      });
+    });
 
-      if (playing && this.state.name == name) {
-        this.myTween.set(this.myWrapper, { y: 15 }).to(this.myWrapper, 0.3, { y: 0, opacity: 1 })
-      } else if (!playing && this.state.name == name) {
-        this.myTween.to(this.myWrapper, 0.3, { y: 15, opacity: 0 })
-      } else {
-        if (playing) {
-          this.myTween.set(this.myWrapper, { opacity: 1 })
+    //Listen for changes
+    songRep.on('change', (newVal, oldVal) => {
+      //Check if song is playing or paused
+      if (oldVal.playing == newVal.playing) {
+        //Don't update if song hasn't changed
+        if (oldVal.name == newVal.name) {
+          return;
         }
 
         this.setState({
-          name,
-          artist,
-          playing,
-        })
+          name: newVal.name,
+          artist: newVal.artist,
+        });
+      } else {
+        this.setState({
+          playing: newVal.playing,
+          name: newVal.name,
+          artist: newVal.artist,
+        });
       }
-    })
+    });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { playing } = this.state;
+
+    if (!prevState.playing && playing) { //If unpaused, show component
+      this.myTween
+        .set(this.myWrapper, { y: 15 })
+        .to(this.myWrapper, 0.3, { y: 0, opacity: 1 });
+    } else if (prevState.playing && !playing) { //If paused, hide component
+      this.myTween.to(this.myWrapper, 0.3, { y: 15, opacity: 0 });
+    }
   }
 
   render() {
@@ -52,8 +78,8 @@ class CurrentSong extends Component {
           {this.state.artist}: <span>{this.state.name}</span>
         </OmniPill>
       </div>
-    )
+    );
   }
 }
 
-export default CurrentSong
+export default CurrentSong;
