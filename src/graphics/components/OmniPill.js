ebf40a0853965795bea0d5bf40a98e6083b24aa5
Mark Markov <digitallymarked@gmail.com>
2019-03-03 00:02:53 +0200

    Fixed state passing from parent and child rendering

--- src/graphics/components/OmniPill.js
+++ src/graphics/components/OmniPill.js
@@ -7,7 +7,7 @@ class OmniPill extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      content: null
+      content: null,
     }
 
     this.myElement = null
@@ -17,35 +17,51 @@ class OmniPill extends Component {
     this.myTween = new TimelineMax()
   }
 
+  componentDidMount() {
+    if (this.props.show == false) {
+      this.myTween.set(this.myElement, { opacity: 0 })
+    }
+  }
+
   componentDidUpdate(oldProps) {
     const newProps = this.props
     const changeState = () => {
       this.setState({ content: newProps.children })
-      console.log(this.myElement.offsetWidth);
     }
-
-    if(oldProps.children !== newProps.children) {
-
-      console.log(this.myElement.offsetWidth);
-      // console.table([oldProps,newProps]);
-      const currentWidth = this.myElement.offsetWidth
-      this.myTween.timeScale(0.1)
+    const myAnimation = () => {
+      this.myTween
         .to(this.myContent, 0.2, {
           y: -15,
           opacity: 0,
           ease: Power2.easeIn,
         })
-        .set(this.myElement, {
-          width: currentWidth
-        })
         .call(changeState)
-        .set(this.myContent, { y: 15,opacity:0})
-        .to(this.myElement, 0.5, { width: "100%"} )
+        .set(this.myContent, { y: 15, opacity: 0 })
         .to(this.myContent, 0.2, {
           y: 0,
           opacity: 1,
           ease: Power2.easeOut,
         })
+    }
+
+    // TODO check why UPDATE is triggering when changin song while paused
+    // Show component
+    if (!oldProps.show && newProps.show) {
+      // If change, update content and then show
+      console.log('Running SHOW')
+      this.myTween
+        .call(changeState)
+        .set(this.myElement, { y: 15 })
+        .to(this.myElement, 0.3, { y: 0, opacity: 1, ease: Power2.easeOut })
+    } else if (oldProps.show == newProps.show && newProps.show && newProps.children != oldProps.children) {
+      console.log('Running UPDATE', oldProps.show, oldProps.children, this.props.show, this.props.children)
+      myAnimation(newProps.children)
+    }
+
+    // Hide component
+    if (oldProps.show && !this.props.show) {
+      console.log('Running HIDE')
+      this.myTween.to(this.myElement, 0.3, { y: 15, opacity: 0, ease: Power2.easeIn })
     }
   }
 
@@ -54,7 +70,9 @@ class OmniPill extends Component {
       <div className="pillWrapper" ref={div => (this.myElement = div)}>
         <div>
           <img className="icon" src={this.props.icon} />
-          <div className="content" ref={div => (this.myContent = div)}>{this.state.content}</div>
+          <div className="content" ref={div => (this.myContent = div)}>
+            {this.state.content}
+          </div>
         </div>
       </div>
     )
--- src/graphics/omnibar/CurrentSong.js
+++ src/graphics/omnibar/CurrentSong.js
@@ -1,85 +1,68 @@
-import React, { Component } from 'react';
-import { TimelineMax, Power2 } from 'gsap/all';
-import OmniPill from '../components/OmniPill';
+import React, { Component } from 'react'
+import OmniPill from '../components/OmniPill'
 
-import music from './icons/music.svg';
+import music from './icons/music.svg'
 
 // NodeCG replicant for connecting to Spotify api - https://github.com/EwanLyon/ncg-spotify
-const songRep = nodecg.Replicant('currentSong', 'ncg-spotify');
+const songRep = nodecg.Replicant('currentSong', 'ncg-spotify')
 
 class CurrentSong extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       name: '',
       artist: '',
       playing: false,
-    };
-
-    // reference to the DOM node
-    this.myWrapper = null;
-
-    // reference to the animation
-    this.myTween = new TimelineMax();
+    }
   }
 
   componentDidMount() {
-    //Wait for Replicant to be declared
+    // Wait for Replicant to be declared
     NodeCG.waitForReplicants(songRep).then(() => {
-      //Load initial state of the items
+      // Load initial state of the items
       nodecg.readReplicant('currentSong', 'ncg-spotify', value => {
         this.setState({
           name: value.name,
           artist: value.artist,
           playing: value.playing,
-        });
-      });
-    });
+        })
+      })
+    })
 
-    //Listen for changes
+    // Listen for changes
     songRep.on('change', (newVal, oldVal) => {
-      //Check if song is playing or paused
-      if (oldVal.playing == newVal.playing) {
-        //Don't update if song hasn't changed
-        if (oldVal.name == newVal.name) {
-          return;
-        }
+      const { name, artist, playing } = newVal
 
-        this.setState({
-          name: newVal.name,
-          artist: newVal.artist,
-        });
+      if (oldVal.playing != playing && playing == true) {
+        this.setState({ playing: true })
+      }
+      if (oldVal.playing != playing && playing == false) {
+        this.setState({ playing: false })
+      }
+
+      // Don't update if song hasn't changed
+      if (oldVal.name == name) {
+        // console.log(name)
+        // return null
       } else {
         this.setState({
-          playing: newVal.playing,
-          name: newVal.name,
-          artist: newVal.artist,
-        });
+          name,
+          artist,
+          // playing,
+        })
       }
-    });
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    const { playing } = this.state;
-
-    if (!prevState.playing && playing) { //If unpaused, show component
-      this.myTween
-        .set(this.myWrapper, { y: 15 })
-        .to(this.myWrapper, 0.3, { y: 0, opacity: 1, ease: Power2.easeOut });
-    } else if (prevState.playing && !playing) { //If paused, hide component
-      this.myTween.to(this.myWrapper, 0.3, { y: 15, opacity: 0, ease: Power2.easeIn });
-    }
+    })
   }
 
   render() {
     return (
       <div id="currentSong" ref={div => (this.myWrapper = div)}>
-        <OmniPill icon={music}>
+        <OmniPill icon={music} show={this.state.playing}>
           {this.state.artist}: <span>{this.state.name}</span>
         </OmniPill>
       </div>
-    );
+    )
   }
 }
 
-export default CurrentSong;
+export default CurrentSong
